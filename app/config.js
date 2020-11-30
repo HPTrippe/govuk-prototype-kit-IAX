412c8d02614a1ba3cffb225d16ab669d217cc759
HPTrippe <72393473+HPTrippe@users.noreply.github.com>
2020-11-30 17:44:24 +0000

    Update Config label

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Join the Identity and Attributes Exchange (IAX)',
+  serviceName: 'Join the Identity and Attributes Exchange',
 
   // Default port that prototype runs on
   port: '3000',
